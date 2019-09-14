267a23d7df149136a991a7e966d683ea83dc7c38
huizhuz <huizhuz@outlook.com>
2019-09-14 15:28:02 -0400

    Updates

---
+++ precache-manifest.b36db8b6d3b2eb38097642a722b4a382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a54a396e993870cfddf7038647c769",
+    "url": "/React-BurgerBuilder/index.html"
+  },
+  {
+    "revision": "e6a3b8050cdfeebec073",
+    "url": "/React-BurgerBuilder/static/css/main.6e987974.chunk.css"
+  },
+  {
+    "revision": "cf5f6913900560d1e145",
+    "url": "/React-BurgerBuilder/static/js/2.41a25232.chunk.js"
+  },
+  {
+    "revision": "e6a3b8050cdfeebec073",
+    "url": "/React-BurgerBuilder/static/js/main.e4141d46.chunk.js"
+  },
+  {
+    "revision": "81e20e7d1b25b1c036f2",
+    "url": "/React-BurgerBuilder/static/js/runtime~main.1f052d15.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/React-BurgerBuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
